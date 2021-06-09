b84dc78d4495559047d48c0ebcb2358e18f9d1dc
bamb4boy <bamb4boy@gmail.com>
2021-06-09 20:19:10 +0300

    tried plan once

--- terraform/main.tf
+++ terraform/main.tf
@@ -93,7 +93,7 @@ module "eks" {
 }
 
 
-
+#module to create pods not yet relevant
 /*provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
